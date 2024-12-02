2752387bf2430096d396b99216cb7339fca4ebfa
Jaya <akulajayateja4444@gmail.com>
2024-12-02 17:00:59 +0530

    the fundamentals quiz section of ReactNative has worked in the frontend

--- src/app.js
+++ src/app.js
@@ -1,11 +1,9 @@
+import { ReactNative_Fundamentals_Quiz_Questions } from "./data/ReactNative.js";
 import express from "express";
 import cors from "cors";
-
-import {
-  ReactNative_Advance_Topics_Quiz_Questions,
-  ReactNative_Intermediate_Topics_Quiz_Questions,
-  ReactNative_Advance_Topics_Quiz_Questions,
-} from "./data/ReactNative_data";
+// const express = require("express");
+// const cors = require("cors");
+// const ReactNative = require("./data/ReactNative_data");
 
 const app = express();
 const PORT = 3000;
@@ -14,26 +12,14 @@ app.use(cors());
 app.use(express.json());
 
 const getQuizData = (language, topic) => {
-  const data = {
-    ReactNative,
-    ReactJS,
-    JavaScript,
-    Java,
-    TypeScript,
-  };
-
-  if (data[language] && data[language][topic]) {
-    return data[language][topic];
-  } else {
-    return null;
-  }
+  return ReactNative_Fundamentals_Quiz_Questions;
 };
 
 app.get("/quiz/:language/:topic", (req, res) => {
   const { language, topic } = req.params;
 
   const formattedTopic = `${topic}_Quiz_Questions`;
-  const quizData = getQuizData(language, formattedTopic);
+  const quizData = getQuizData(language);
 
   if (quizData) {
     res.json(quizData);
