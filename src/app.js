135e58501f959f6a5207340c8075cf32678d86f6
Jaya <akulajayateja4444@gmail.com>
2024-12-04 11:59:39 +0530

    data of entire ReactNative quiz section is fetched from the fronted and it worked.

--- src/app.js
+++ src/app.js
@@ -1,4 +1,8 @@
-import { ReactNative_Fundamentals_Quiz_Questions } from "./data/ReactNative.js";
+import {
+  ReactNative_Fundamentals_Quiz_Questions,
+  ReactNative_Intermediate_Topics_Quiz_Questions,
+  ReactNative_Advance_Topics_Quiz_Questions,
+} from "./data/ReactNative.js";
 import express from "express";
 import cors from "cors";
 // const express = require("express");
@@ -11,23 +15,47 @@ const PORT = 3000;
 app.use(cors());
 app.use(express.json());
 
-const getQuizData = (language, topic) => {
-  return ReactNative_Fundamentals_Quiz_Questions;
+// const getQuizData = (language, topic) => {
+//   return ReactNative_Fundamentals_Quiz_Questions;
+// };
+
+// app.get("/quiz/:language/:topic", (req, res) => {
+//   const { language, topic } = req.params;
+
+//   const formattedTopic = `${topic}_Quiz_Questions`;
+//   const quizData = getQuizData(language);
+
+//   if (quizData) {
+//     res.json(quizData);
+//   } else {
+//     res
+//       .status(404)
+//       .json({ message: `No quiz data found for ${language} - ${topic}` });
+//   }
+// });
+
+const quizData = {
+  ReactNative: {
+    fundamentals: ReactNative_Fundamentals_Quiz_Questions,
+    intermediate: ReactNative_Intermediate_Topics_Quiz_Questions,
+    advanced: ReactNative_Advance_Topics_Quiz_Questions,
+  },
 };
 
-app.get("/quiz/:language/:topic", (req, res) => {
-  const { language, topic } = req.params;
+app.get("/quiz/:language", (req, res) => {
+  const { language } = req.params;
 
-  const formattedTopic = `${topic}_Quiz_Questions`;
-  const quizData = getQuizData(language);
-
-  if (quizData) {
-    res.json(quizData);
-  } else {
-    res
-      .status(404)
-      .json({ message: `No quiz data found for ${language} - ${topic}` });
+  if (!quizData[language]) {
+    return res.status(404).json({ message: `Language ${language} not found!` });
   }
+
+  const allQuestions = {
+    fundamentals: quizData[language].fundamentals,
+    intermediate: quizData[language].intermediate,
+    advanced: quizData[language].advanced,
+  };
+
+  res.json(allQuestions);
 });
 
 app.listen(PORT, () => {
